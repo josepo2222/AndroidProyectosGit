61a747917b20347891557a2b3284e9f18f459391
josepo2222 <josepo2000@gmail.com>
2017-02-09 21:31:53 +0100

    cosasv

---
+++ buscabusca/app/src/main/java/com/a1apli/a8fprogmm08/buscabusca/MainActivity.java
@@ -0,0 +1,70 @@
+package com.a1apli.a8fprogmm08.buscabusca;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch(item.getItemId()){
+            case R.id.InstruccionesB:
+               // mostrarInstr();
+                return true;
+            case R.id.nuevoJuegoB:
+
+                return true;
+
+            case R.id.dificultadB:
+
+                return true;
+            case R.id.personajesB:
+
+                return true;
+
+            default:
+                return super.onOptionsItemSelected(item);
+
+        }
+
+
+
+
+    }
+}
